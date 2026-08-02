4f9d0715e2195ffd94504ddf4720ef12b04a7736
Nitheesvar10 <150546091+Nitheesvar10@users.noreply.github.com>
2026-08-02 15:29:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Parenthesis Checker/parenthesis-checker.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isBalanced(self, s):
+        stack = []
+
+        for ch in s:
+            if ch in "([{":
+                stack.append(ch)
+            else:
+                if not stack:
+                    return False
+
+                if ch == ")" and stack[-1] == "(":
+                    stack.pop()
+                elif ch == "]" and stack[-1] == "[":
+                    stack.pop()
+                elif ch == "}" and stack[-1] == "{":
+                    stack.pop()
+    
+                else:
+                    return False 
+
+        return len(stack) == 0

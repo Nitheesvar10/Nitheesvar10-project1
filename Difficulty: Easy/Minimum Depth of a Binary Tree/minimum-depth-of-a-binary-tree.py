b6d08fe5e530667b2fab1afcc6feb5db3c2ce3f2
Nitheesvar10 <150546091+Nitheesvar10@users.noreply.github.com>
2026-08-04 16:39:32 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Minimum Depth of a Binary Tree/minimum-depth-of-a-binary-tree.py
@@ -0,0 +1,29 @@
+'''
+class Node:
+    def __init__(self, val):
+        self.right = None
+        self.data = val
+        self.left = None
+'''
+
+class Solution:
+    def solve(self,node):
+        
+        if node is None:
+            return 0
+            
+        if node.left is None:
+            return 1+self.solve(node.right)
+        if node.right is None:
+            return 1+self.solve(node.left) 
+            
+        left=self.solve(node.left)
+        right=self.solve(node.right)
+        return 1+min(left,right)
+        
+        
+        
+        
+    def minDepth(self, root):
+        #Code here
+        return self.solve(root)

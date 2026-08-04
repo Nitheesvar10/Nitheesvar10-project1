01e7e507d1d6852c89020eeb51ff48ddf290aa86
Nitheesvar10 <150546091+Nitheesvar10@users.noreply.github.com>
2026-08-04 15:42:09 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Diameter of a Binary Tree/diameter-of-a-binary-tree.py
@@ -0,0 +1,27 @@
+''' Structure of binary tree Node 
+class Node:
+    def __init__(self, val):
+        self.data = val
+        self.right = None
+        self.left = None
+'''
+
+class Solution:
+    def solve(self,node):
+        if node is None:
+            return 0
+            
+        left=self.solve(node.left)
+        right=self.solve(node.right)
+        self.diameter=max(self.diameter,left+right)
+        return 1+max(left,right)
+    def diameter(self, root):
+        # code here
+        self.diameter=0
+        self.solve(root)
+        
+        return self.diameter 
+        
+        
+        
+        

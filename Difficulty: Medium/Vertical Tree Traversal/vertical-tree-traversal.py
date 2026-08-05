ea7aabb9f50ec60eaca85e9d71e156c7baac13c4
Nitheesvar10 <150546091+Nitheesvar10@users.noreply.github.com>
2026-08-05 16:51:21 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Vertical Tree Traversal/vertical-tree-traversal.py
+++ Difficulty: Medium/Vertical Tree Traversal/vertical-tree-traversal.py
@@ -1,30 +1,39 @@
+''' Structure of binary tree node
+class Node:
+    def __init__(self, val):
+        self.data = val
+        self.left = None
+        self.right = None
+'''
+
 class Solution:
-    def verticalOrder(self, root):
-        nodes = []
-        cnt = 0
-
-        def dfs(node, row, col):
+    def verticalOrder(self, root): 
+        # code here
+        nodes=[]
+        cnt=0
+        
+        def dfs(node,row,col):
             nonlocal cnt
+            
             if node is None:
-                return
-
-            nodes.append((col, row, cnt, node.data))
-            cnt += 1
-
-            dfs(node.left, row + 1, col - 1)
-            dfs(node.right, row + 1, col + 1)
-
-        dfs(root, 0, 0)
-
-        nodes.sort(key=lambda x: (x[0], x[1], x[2]))
-
-        ans = []
-        prevcol = float("-inf")
-
-        for col, row, order, val in nodes:
-            if col != prevcol:
+                return 
+            
+            nodes.append([col,row,cnt,node.data])
+            
+            dfs(node.left,row+1,col-1)
+            dfs(node.right,row+1,col+1)
+            
+        dfs(root,0,0)
+        
+        nodes.sort(key=lambda x:(x[0],x[1],x[2]))
+        ans=[]
+        prev=float("-inf")
+        for col,row,order,val in nodes:
+            if prev!=col:
                 ans.append([])
-                prevcol = col
+                prev=col
             ans[-1].append(val)
-
-        return ans
+            
+        return ans 
+            
+        

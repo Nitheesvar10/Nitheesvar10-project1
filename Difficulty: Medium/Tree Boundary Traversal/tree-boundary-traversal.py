85849a014539e4ab880c1738473ec39dc02bd14b
Nitheesvar10 <150546091+Nitheesvar10@users.noreply.github.com>
2026-08-05 19:30:50 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Tree Boundary Traversal/tree-boundary-traversal.py
+++ Difficulty: Medium/Tree Boundary Traversal/tree-boundary-traversal.py
@@ -8,27 +8,31 @@ class Node:
 '''
 
 class Solution:
-    def leafnode(self,node):
-        if node.left is None and node.right is None:
-            return True
-        return False
-        
+    
+    def isleaf(self,node):
+        if  node.left is None and node.right is None:
+            return True 
+        return False 
+    
     def left(self,node):
         if node is None:
             return 
-        if not (self.leafnode(node)):
+        
+        if not(self.isleaf(node)):
             self.result.append(node.data)
+        
         if node.left:
             self.left(node.left)
-            
         else:
             self.left(node.right)
-        
-    def leaf(self,node):
-        if not node:
-            return 
             
-        if self.leafnode(node):
+        
+            
+    def leaf(self,node):
+        if node is None:
+            return 
+        
+        if self.isleaf(node):
             self.result.append(node.data)
             
         self.leaf(node.left)
@@ -39,33 +43,24 @@ class Solution:
         if node is None:
             return 
         
-        
         if node.right:
             self.right(node.right)
         else:
             self.right(node.left)
             
-        if not(self.leafnode(node)):
+        if not(self.isleaf(node)):
             self.result.append(node.data)
-            
-        
-    
-        
-        
             
             
     def boundaryTraversal(self, root):
+        # code here
         if root.left is None and root.right is None:
             return [root.data]
-        # code here
-        self.result = [root.data]
-
+        self.result=[]
+        self.result.append(root.data)
         self.left(root.left)
         self.leaf(root)
         self.right(root.right)
         
         return self.result 
-                
-        
-               
         

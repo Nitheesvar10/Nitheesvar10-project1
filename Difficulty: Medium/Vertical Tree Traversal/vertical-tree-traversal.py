91149d749b03742a8d676f13c03ab036a41fb757
Nitheesvar10 <150546091+Nitheesvar10@users.noreply.github.com>
2026-08-05 18:22:49 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Vertical Tree Traversal/vertical-tree-traversal.py
+++ Difficulty: Medium/Vertical Tree Traversal/vertical-tree-traversal.py
@@ -5,35 +5,37 @@ class Node:
         self.left = None
         self.right = None
 '''
-
+# from colllections import deque
 class Solution:
+    
+    def dfs(self,node,row,col):
+        if node is None:
+            return 
+        
+        self.nodes.append([col,row,self.cnt,node.data])
+        self.cnt+=1
+        
+        
+        self.dfs(node.left,row+1,col-1)
+        self.dfs(node.right,row+1,col+1)
+        
     def verticalOrder(self, root): 
         # code here
-        nodes=[]
-        cnt=0
-        
-        def dfs(node,row,col):
-            nonlocal cnt
-            
-            if node is None:
-                return 
-            
-            nodes.append([col,row,cnt,node.data])
-            
-            dfs(node.left,row+1,col-1)
-            dfs(node.right,row+1,col+1)
-            
-        dfs(root,0,0)
-        
-        nodes.sort(key=lambda x:(x[0],x[1],x[2]))
+
+        self.cnt=0
+        self.nodes=[]
+        self.dfs(root,0,0)
+        self.nodes.sort(key=lambda x:(x[0],x[1],x[2]))
         ans=[]
         prev=float("-inf")
-        for col,row,order,val in nodes:
+        
+        for col,row,order,val in self.nodes:
             if prev!=col:
                 ans.append([])
                 prev=col
+                
             ans[-1].append(val)
             
         return ans 
-            
+        
         

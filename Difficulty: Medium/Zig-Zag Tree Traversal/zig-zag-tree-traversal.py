72f8a6eccc79f61416824ba717c5c6d94c4336e6
Nitheesvar10 <150546091+Nitheesvar10@users.noreply.github.com>
2026-08-05 12:28:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Zig-Zag Tree Traversal/zig-zag-tree-traversal.py
+++ Difficulty: Medium/Zig-Zag Tree Traversal/zig-zag-tree-traversal.py
@@ -1,32 +1,37 @@
+''' Structure of Binary Tree Node
+class Node:
+    def __init__(self, val):
+        self.data = val
+        self.left = None
+        self.right = None
+'''
 from collections import deque
-
 class Solution:
     def zigZagTraversal(self, root):
-        if root is None:
-            return []
-
-        q = deque([root])
-        result = []
-        left_to_right = True
-
-        while q:
-            level = deque()
-            size = len(q)
-
-            for _ in range(size):
-                node = q.popleft()
-
-                if left_to_right:
-                    level.append(node.data)
+        q1=deque([root])
+        result=[]
+        flag=True
+        while q1:
+            q2=deque([])
+            l=len(q1)
+            for _ in range(l):
+                node=q1.popleft()
+                
+                if flag:
+                    q2.append(node.data)
                 else:
-                    level.appendleft(node.data)
-
+                
+                    q2.appendleft(node.data)
                 if node.left:
-                    q.append(node.left)
+                    q1.append(node.left)
                 if node.right:
-                    q.append(node.right)
-
-            result.extend(level)
-            left_to_right = not left_to_right
-
-        return result
+                    q1.append(node.right)
+                    
+            flag=not flag
+            result.extend(q2)
+            
+        return result 
+            
+                
+            
+        

dfdf2a551fac144231ab6eaad9fbb6808aacf6db
Nitheesvar10 <150546091+Nitheesvar10@users.noreply.github.com>
2026-08-06 20:48:39 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find Kth Rotation/find-kth-rotation.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findKRotation(self, arr):
+        # code here
+        left=0
+        right=len(arr)-1
+        while (left<right):
+            mid=left+(right-left)//2
+            
+            if arr[mid]>arr[right]:
+                left=mid+1
+            else:
+                right=mid
+                
+        return left
+        

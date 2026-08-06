77b5b998e36e945ea0c008571580b77c1e8d0b50
Nitheesvar10 <150546091+Nitheesvar10@users.noreply.github.com>
2026-08-06 20:36:56 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Sorted and Rotated Minimum/sorted-and-rotated-minimum.py
@@ -0,0 +1,18 @@
+class Solution:
+    def findMin(self, arr):
+        # code here
+        left=0
+        right=len(arr)-1
+        
+        while (left<right):
+            mid=left+(right-left)//2
+            
+            if arr[mid]>arr[right]:
+                left=mid+1
+                
+            else:
+                right=mid
+                
+        return arr[left]
+                
+    

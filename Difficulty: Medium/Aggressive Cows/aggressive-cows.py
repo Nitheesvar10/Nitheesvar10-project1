925828f4f08588bf702d2e56852a5a2c6f8e1950
Nitheesvar10 <150546091+Nitheesvar10@users.noreply.github.com>
2026-08-07 16:24:30 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Aggressive Cows/aggressive-cows.py
+++ Difficulty: Medium/Aggressive Cows/aggressive-cows.py
@@ -2,28 +2,26 @@ class Solution:
     def aggressiveCows(self, arr, k):
         # code here
         arr.sort()
-        
         low=1
         high=arr[len(arr)-1]-arr[0]
         
         while low<=high:
-            pos=low+(high-low)//2
+            spot=low+(high-low)//2
             
-            if self.proper(arr,k,pos):
-                ans=pos
-                low=pos+1
+            if self.spotted(arr,k,spot):
+                ans=spot
+                low=spot+1
             else:
-                high=pos-1
+                high=spot-1
                 
-        return ans 
+        return ans
         
-    def proper(self,arr,k,pos):
+    def spotted(self,arr,k,spot):
         count=1
-        last_pos=arr[0]
-        
-        for i in range (1,len(arr)):
-            if arr[i]-last_pos >=pos:
+        last=arr[0]
+        for i in range(1,len(arr)):
+            if arr[i]-last>=spot:
                 count+=1
-                last_pos=arr[i]
+                last=arr[i]
                 
-        return  count>=k
+        return count>=k

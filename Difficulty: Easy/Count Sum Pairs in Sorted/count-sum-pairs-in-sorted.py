4234147f5717e17f5f04490f1497e27eb52cd10b
Nitheesvar10 <150546091+Nitheesvar10@users.noreply.github.com>
2026-08-07 22:43:37 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Count Sum Pairs in Sorted/count-sum-pairs-in-sorted.py
@@ -0,0 +1,39 @@
+class Solution:
+    def countPairs(self, arr, target): 
+        
+        #  Code Here
+        count=0
+        left=0
+        right=len(arr)-1
+        while left<=right:
+            summ=arr[left]+arr[right]
+            
+            if summ>target:
+                right-=1
+            elif summ<target:
+                left+=1
+                
+            else:
+                
+                if arr[left]==arr[right]:
+                    n=right-left+1
+                    count+=n*(n-1)//2
+                    break
+                
+                left_count=1
+                while left+1< right and arr[left]==arr[left+1]:
+                    left_count+=1
+                    left+=1
+                    
+                    
+                right_count=1
+                while right-1>left and arr[right]==arr[right-1]:
+                    right_count+=1
+                    right-=1
+                    
+                count+=left_count*right_count
+                left+=1
+                right-=1
+                
+        return count
+                

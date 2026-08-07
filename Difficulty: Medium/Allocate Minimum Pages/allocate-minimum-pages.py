a93a625bbe743b701c4b35c6cba33c71f860ef9d
Nitheesvar10 <150546091+Nitheesvar10@users.noreply.github.com>
2026-08-07 18:18:00 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Allocate Minimum Pages/allocate-minimum-pages.py
+++ Difficulty: Medium/Allocate Minimum Pages/allocate-minimum-pages.py
@@ -1,33 +1,33 @@
 class Solution:
     def findPages(self, arr, k):
-        # If students are more than books, allocation is impossible
-        if k > len(arr):
+        # code here
+        if k>len(arr):
             return -1
-
-        low = max(arr)
-        high = sum(arr)
-        ans = -1
-
-        while low <= high:
-            allot = low + (high - low) // 2
-
-            if self.ok(arr, k, allot):
-                ans = allot
-                high = allot - 1
+            
+        low=max(arr)
+        
+        high=sum(arr)
+        
+        while low <=high:
+            
+            pages=low+(high-low)//2
+            
+            if self.ok(arr,k,pages):
+                ans=pages
+                high=pages-1
+                
             else:
-                low = allot + 1
-
-        return ans
-
-    def ok(self, arr, k, allot):
-        count = 1
-        cur_page_count = 0
-
-        for pages in arr:
-            if cur_page_count + pages > allot:
-                count += 1
-                cur_page_count = pages
+                low=pages+1
+                
+        return ans  
+    def ok(self,arr,k,pages):
+        count=1
+        cur_count=0
+        for i in range(len(arr)):
+            if cur_count+arr[i]>pages:
+                count+=1
+                cur_count=arr[i]
             else:
-                cur_page_count += pages
-
-        return count <= k
+                cur_count+=arr[i]
+                
+        return  count<=k

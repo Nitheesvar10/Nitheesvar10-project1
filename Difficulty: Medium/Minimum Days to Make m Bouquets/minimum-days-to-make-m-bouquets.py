cac16253efedc300c307a144ac4eba14309d06d5
Nitheesvar10 <150546091+Nitheesvar10@users.noreply.github.com>
2026-08-07 15:01:40 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Days to Make m Bouquets/minimum-days-to-make-m-bouquets.py
+++ Difficulty: Medium/Minimum Days to Make m Bouquets/minimum-days-to-make-m-bouquets.py
@@ -1,34 +1,37 @@
 class Solution:
     def minDaysBloom(self, arr, k, m):
         # Code here
+        
         if k*m >len(arr):
             return -1
             
         low=min(arr)
         high=max(arr)
-        ans=high
-        
-        while (low<=high):
+    
+        while low<=high:
             day=low+(high-low)//2
             
-            if self.bloom(arr,k,m,day):
-                ans=day 
+            if self.canmake(arr,m,k,day):
+                ans=day
                 high=day-1
                 
             else:
                 low=day+1
                 
-        return ans 
+        return ans
         
-    def bloom(self,arr,k,m,day):
-        b=0
+    def canmake(self,arr,m,k,day):
         count=0
+        b=0
         for i in arr:
             if i<=day:
                 count+=1
-                if count==k:
+                if count>=k:
                     b+=1
                     count=0
             else:
                 count=0
-        return b>=m
+        return  b>=m
+            
+        
+        

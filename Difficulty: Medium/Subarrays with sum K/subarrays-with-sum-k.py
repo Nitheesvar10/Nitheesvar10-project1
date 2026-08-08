a1df6547a0107c8efbb013d6aee1d84c03682368
Nitheesvar10 <150546091+Nitheesvar10@users.noreply.github.com>
2026-08-08 16:51:19 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Subarrays with sum K/subarrays-with-sum-k.py
@@ -0,0 +1,16 @@
+class Solution:
+    def cntSubarrays(self, arr, k):
+        # code here
+        cnt=0
+        hash={0:1}
+        cur_sum=0
+        for i in range(len(arr)):
+            cur_sum+=arr[i]
+            e=cur_sum-k
+            
+            if e in hash:
+                cnt+=hash[e]
+                
+            hash[cur_sum]=hash.get(cur_sum,0)+1
+            
+        return cnt

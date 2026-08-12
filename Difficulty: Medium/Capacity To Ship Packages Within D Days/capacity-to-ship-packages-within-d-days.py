1cf0ff2e6bedd980eb4b0659adab3f3061426f91
Nitheesvar10 <150546091+Nitheesvar10@users.noreply.github.com>
2026-08-12 18:10:42 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Capacity To Ship Packages Within D Days/capacity-to-ship-packages-within-d-days.py
+++ Difficulty: Medium/Capacity To Ship Packages Within D Days/capacity-to-ship-packages-within-d-days.py
@@ -3,30 +3,31 @@ class Solution:
         # code here
         low=max(arr)
         high=sum(arr)
-        ans=high
-        while low<=high:
-            cap=low+(high-low)//2
-            
-            if self.capable(arr,D,cap):
-                ans=cap
-                high=cap-1
-                
-            else:
-                low=cap+1
-        return ans 
-    
-    def capable(self,arr,D,cap):
-        d=1
-        c=0
-        for i in arr:
-            if c+i>cap:
-                d+=1
-                c=i
-            else:
-                c+=i
-        return d<=D
+        ans=None 
         
-                
+        while low<=high:
+            mid=low+(high-low)//2
             
+            if self.go(arr,mid,D):
+                ans=mid
+                high=mid-1 
+                
+            else:
+                low=mid+1
+        return ans 
+        
+    def go(self,arr,w,D):
+        weight=0
+        d=1
+        for i in arr:
+            
+            if (weight+i)<=w:
+                weight+=i
+                
+            else:
+                weight=i
+                d+=1
+        
+        return d<=D
             
             

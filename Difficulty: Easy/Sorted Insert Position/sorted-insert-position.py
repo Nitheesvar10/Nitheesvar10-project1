36bc78ba48159f05d950de28062c13011cd90506
Nitheesvar10 <150546091+Nitheesvar10@users.noreply.github.com>
2026-08-12 14:18:56 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Sorted Insert Position/sorted-insert-position.py
+++ Difficulty: Easy/Sorted Insert Position/sorted-insert-position.py
@@ -3,13 +3,15 @@ class Solution:
         # code here
         left=0
         right=len(arr)-1
-        while (left<=right):
+        
+        
+        while left <=right:
             mid=left+(right-left)//2
             
             if arr[mid]==k:
                 return mid
                 
-            if arr[mid]<k:
+            elif arr[mid]<k:
                 left=mid+1
                 
             else:

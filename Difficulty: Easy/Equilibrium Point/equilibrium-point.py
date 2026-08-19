727934ba39783bac4ba50a1170596664ba1d6152
Nitheesvar10 <150546091+Nitheesvar10@users.noreply.github.com>
2026-08-19 14:33:54 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Equilibrium Point/equilibrium-point.py
+++ Difficulty: Easy/Equilibrium Point/equilibrium-point.py
@@ -1,20 +1,21 @@
 class Solution:
     def findEquilibrium(self, arr):
         # code here
-        pre_sum=[0]*len(arr)
-        suf_sum=[0]*len(arr)
-        pre_sum[0]=arr[0]
         
+        pref=[0]*len(arr)
+        suf=[0]*len(arr)
+        pref[0]=arr[0]
+        suf[len(arr)-1]=arr[len(arr)-1]
+
+
         for i in range(1,len(arr)):
-            pre_sum[i]=pre_sum[i-1]+arr[i]
+            pref[i]=arr[i]+pref[i-1]
             
-        suf_sum[len(arr)-1]=arr[len(arr)-1]
-        
         for i in range(len(arr)-2,-1,-1):
-            suf_sum[i]=suf_sum[i+1]+arr[i]
+            suf[i]=arr[i]+suf[i+1]
             
-        for i in range(len(arr)):
-            if pre_sum[i]==suf_sum[i]:
-                return i 
+        for i in range(0,len(arr)):
+            if pref[i]==suf[i]:
+                return i
         
-        return -1
+        return -1 

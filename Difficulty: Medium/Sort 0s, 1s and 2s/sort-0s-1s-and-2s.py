95a1282f0788144e3c4d78d217935814f8766511
Nitheesvar10 <150546091+Nitheesvar10@users.noreply.github.com>
2026-08-19 13:31:57 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.py
+++ Difficulty: Medium/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.py
@@ -4,15 +4,18 @@ class Solution:
         left=0
         mid=0
         right=len(arr)-1
-        while right >=mid:
+        
+        while mid<=right:
             if arr[mid]==0:
-                arr[mid],arr[left]=arr[left],arr[mid]
-                mid+=1
+                arr[left],arr[mid]=arr[mid],arr[left]
                 left+=1
+                mid+=1
             elif arr[mid]==1:
                 mid+=1
+                continue 
             else:
                 arr[mid],arr[right]=arr[right],arr[mid]
                 right-=1
+        return arr 
+                
             
-        return arr

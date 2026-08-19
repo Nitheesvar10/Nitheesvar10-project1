8b60df2087a58fe848356ec0fff238362733ceee
Nitheesvar10 <150546091+Nitheesvar10@users.noreply.github.com>
2026-08-19 14:15:06 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest subarray with Atmost two distinct integers/longest-subarray-with-atmost-two-distinct-integers.py
+++ Difficulty: Medium/Longest subarray with Atmost two distinct integers/longest-subarray-with-atmost-two-distinct-integers.py
@@ -3,21 +3,21 @@ class Solution:
         # Code here
         dict={}
         left=0
-        max_cnt=0
         right=0
-        while right<len(arr):
+        max_cnt=0
+        while right <len(arr):
             dict[arr[right]]=dict.get(arr[right],0)+1
-                
+            
             while len(dict)>2:
+                
                 dict[arr[left]]-=1
                 if dict[arr[left]]==0:
                     del dict[arr[left]]
                 left+=1
-            
-            if len(dict)<=2:
-                max_cnt=max(max_cnt,right-left+1)
                 
+            max_cnt=max(max_cnt,right-left+1)
+            
             right+=1
         return max_cnt
-                
+            
                 

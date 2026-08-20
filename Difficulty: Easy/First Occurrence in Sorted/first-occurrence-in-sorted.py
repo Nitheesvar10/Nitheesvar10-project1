afa05e21f0e5642bddfa1923d396e081c7181a32
Nitheesvar10 <150546091+Nitheesvar10@users.noreply.github.com>
2026-08-20 18:25:49 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/First Occurrence in Sorted/first-occurrence-in-sorted.py
+++ Difficulty: Easy/First Occurrence in Sorted/first-occurrence-in-sorted.py
@@ -3,18 +3,18 @@ class Solution:
         # Code Here
         left=0
         right=len(arr)-1
-        ans=None
-        while left<=right:
-            mid=left+(right-left)//2
+        ans=-1
+        while left <=right:
+            mid=(left+(right-left)//2)
             
             if arr[mid]==k:
-                ans=mid
-                right=mid-1 
-                
-            elif arr[mid]>k:
+                ans= mid
                 right=mid-1
                 
-            else:
+            elif arr[mid]<k:
                 left=mid+1
                 
-        return ans if ans  is not None else -1
+            else:
+                right=mid-1
+            
+        return ans

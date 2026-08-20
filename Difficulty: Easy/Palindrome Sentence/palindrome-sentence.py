f4448ba9d2dc7a3b299e369c7b85076e29728564
Nitheesvar10 <150546091+Nitheesvar10@users.noreply.github.com>
2026-08-20 17:14:31 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Palindrome Sentence/palindrome-sentence.py
+++ Difficulty: Easy/Palindrome Sentence/palindrome-sentence.py
@@ -1,22 +1,21 @@
 class Solution:
-    def isPalinSent(self, s):
-        left = 0
-        right = len(s) - 1
-
-        while left < right:
-
-            # Skip non-alphanumeric characters
-            while left < right and not s[left].isalnum():
-                left += 1
-
-            while left < right and not s[right].isalnum():
-                right -= 1
-
-            # Compare valid characters
-            if s[left].lower() != s[right].lower():
-                return False
-
-            left += 1
-            right -= 1
-
-        return True
+	def isPalinSent(self, s):
+		# code here
+		left=0
+		right=len(s)-1
+		while left<=right:
+		    if not s[left].isalnum():
+		        left+=1
+		        continue
+		        
+		    if not s[right].isalnum():
+		        right-=1
+		        continue 
+		    if s[left].lower() != s[right].lower():
+		        return False 
+		        
+		    
+		    left+=1
+		    right-=1
+		    
+	    return True 

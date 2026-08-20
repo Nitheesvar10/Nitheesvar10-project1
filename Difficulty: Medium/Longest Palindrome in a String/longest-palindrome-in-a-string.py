68166f1c99742edabc2b424776792e6ed48f15de
Nitheesvar10 <150546091+Nitheesvar10@users.noreply.github.com>
2026-08-20 17:31:07 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Palindrome in a String/longest-palindrome-in-a-string.py
+++ Difficulty: Medium/Longest Palindrome in a String/longest-palindrome-in-a-string.py
@@ -6,19 +6,21 @@ class Solution:
         end=0
         
         for i in range(len(s)):
-            even_pal=self.pal(s,i,i+1)
-            odd_pal=self.pal(s,i,i)
+            odd_len=self.pal(s,i,i)
+            even_len=self.pal(s,i,i+1)
             
-            max_lenght=max(even_pal,odd_pal)
+            max_lenght=max(even_len,odd_len)
             
             if max_lenght>(end-start+1):
                 start=i-(max_lenght-1)//2
                 end=i+(max_lenght)//2
                 
         return s[start:end+1]
-        
+            
     def pal(self,s,left,right):
-        while left>=0 and right <len(s) and s[left]==s[right]:
+        while (left >=0 and right <len(s) )and s[left]==s[right]:
             left-=1
             right+=1
-        return  right-left-1
+            
+            
+        return right-left-1

1e9fb06a8a2ae4ef83a167176ddda0a6b7438bd9
Nitheesvar10 <150546091+Nitheesvar10@users.noreply.github.com>
2026-08-22 16:32:29 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Identical Trees/identical-trees.py
+++ Difficulty: Easy/Identical Trees/identical-trees.py
@@ -6,25 +6,28 @@ class Node:
         self.left = None
 '''
 
+
 class Solution:
-    def solve(self,node,result):
+    def dfs(self,node,result):
         if node is None:
             return None
+            
+            
+        self.dfs(node.left,result)
+        self.dfs(node.right,result)
         result.append(node.data)
-        self.solve(node.left,result)
-        self.solve(node.right,result)
-    
+        
     def isIdentical(self, r1, r2):
         # code here
-        self.result1=[]
-        self.result2=[]
-        self.solve(r1,self.result1)
-        self.solve(r2,self.result2)
-        if self.result1==self.result2:
-            return True 
+        
+        result1=[]
+        result2=[]
+        
+        self.dfs(r1,result1)
+        self.dfs(r2,result2)
+        
+        if result1==result2:
+            return True  
         else:
-            return False 
-            
-        
-        
+            return False
         

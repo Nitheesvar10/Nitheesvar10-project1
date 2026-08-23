fd10bff4706a350edd0e8394118af34a655e9de9
Nitheesvar10 <150546091+Nitheesvar10@users.noreply.github.com>
2026-08-23 11:02:29 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Generate all binary strings/generate-all-binary-strings.py
+++ Difficulty: Medium/Generate all binary strings/generate-all-binary-strings.py
@@ -1,22 +1,16 @@
 class Solution:
     def solve(self,arr,idx):
-        if idx>=self.n:
-            self.result.append("".join(arr))
+        if idx==len(arr):
+            self.result.append("".join(arr.copy()))
             return 
         arr[idx]="0"
         self.solve(arr,idx+1)
         arr[idx]="1"
         self.solve(arr,idx+1)
-        
-
-    
     def binstr(self, n):
         # code here
-        self.n=n
-        self.result=[]
         arr=["0"]*n
-        
+        self.result=[]
         self.solve(arr,0)
         
         return self.result 
-        

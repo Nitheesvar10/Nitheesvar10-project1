0492f2e98b4a772b107f2bc1043ea20538beb525
Nitheesvar10 <150546091+Nitheesvar10@users.noreply.github.com>
2026-08-23 09:53:43 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Subsets/subsets.py
+++ Difficulty: Medium/Subsets/subsets.py
@@ -1,18 +1,19 @@
 class Solution:
-    def solve(self, idx, subset, arr):
-        if idx == len(arr):
+    def solve(self,arr,idx,subset):
+        
+        if idx==len(arr):
             self.result.append(subset.copy())
-            return
-
+            return 
+        
         subset.append(arr[idx])
-        self.solve(idx + 1, subset, arr)
-
+        self.solve(arr,idx+1,subset)
         subset.pop()
-        self.solve(idx + 1, subset, arr)
-
+        self.solve(arr,idx+1,subset)
+        
     def subsets(self, arr):
-        self.result = []
-        subset = []
-
-        self.solve(0, subset, arr)
+        self.lenght=len(arr)
+        self.result=[]
+        self.solve(arr,0,[])
+        
         return self.result
+        

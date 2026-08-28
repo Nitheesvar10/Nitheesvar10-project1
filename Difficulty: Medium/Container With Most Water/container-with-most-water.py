4a44622439eb0f525fa1baba33a9fd80eae2da13
Nitheesvar10 <150546091+Nitheesvar10@users.noreply.github.com>
2026-08-28 21:34:53 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Container With Most Water/container-with-most-water.py
+++ Difficulty: Medium/Container With Most Water/container-with-most-water.py
@@ -2,18 +2,27 @@ class Solution:
     def maxWater(self, arr):
         # code here
         left=0
-        right =len(arr)-1
-        max_area=0
+        right=len(arr)-1
+        max_area=float("-inf")
+        
         while left<right:
-            width=right-left
-            lenght=min(arr[left],arr[right])
-            area=lenght*width
-            max_area =max(max_area,area)
             
+            widht=right-left
+            
+            lenght=min(arr[left],arr[right])
+            area=widht*lenght
+             
+            max_area=max(area,max_area)
+             
             if arr[left]<arr[right]:
                 left+=1
+                
             else:
                 right-=1
                 
-        return max_area
+            
+                 
+        return max_area  if max_area!= float("-inf") else 0
+                
+            
             

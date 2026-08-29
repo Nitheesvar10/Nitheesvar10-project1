26b6c82ff36ca0ee9a6f536622d96fd69244881f
Nitheesvar10 <150546091+Nitheesvar10@users.noreply.github.com>
2026-08-29 12:42:59 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Subarrays Product Less than K/subarrays-product-less-than-k.py
+++ Difficulty: Medium/Subarrays Product Less than K/subarrays-product-less-than-k.py
@@ -2,23 +2,27 @@ class Solution:
     def countSubarray(self, arr, k):
         # code here
         if k<=1:
-            return 0 
-            
+            return 0
         left=0
         right=0
+        cnt=0
         cur_prod=1
-        count=0
-        while right<len(arr):
+        
+        while right <len(arr):
             cur_prod*=arr[right]
             
-            while cur_prod >=k:
-                cur_prod=cur_prod//arr[left]
-                
-                left+=1
             
-            count+=right-left+1
+            while cur_prod >=k:
+                cur_prod/=arr[left]
+                left+=1
+            if cur_prod <k:
+                
+                cnt+=right-left+1
+            
             
             right+=1
-        
-        return count 
+            
+            
+        return cnt
+                
                 

0f17bfa4b3e9328fb127b8ed495e2c0732c9200b
Nitheesvar10 <150546091+Nitheesvar10@users.noreply.github.com>
2026-08-30 18:47:34 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Find Kth Rotation/find-kth-rotation.py
+++ Difficulty: Easy/Find Kth Rotation/find-kth-rotation.py
@@ -3,12 +3,13 @@ class Solution:
         # code here
         left=0
         right=len(arr)-1
-        while left< right :
-            mid=left+(right-left)//2 
+        
+        while left<right:
+            mid=left+(right-left)//2
             
             if arr[mid]>arr[right]:
                 left=mid+1
-                
             else:
                 right=mid
-        return left 
+                
+        return left

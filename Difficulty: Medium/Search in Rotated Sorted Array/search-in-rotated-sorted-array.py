8765755a3f6cc1378ea35a6b53bb0b6b4fb0f7ef
Nitheesvar10 <150546091+Nitheesvar10@users.noreply.github.com>
2026-08-30 17:42:05 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Search in Rotated Sorted Array/search-in-rotated-sorted-array.py
+++ Difficulty: Medium/Search in Rotated Sorted Array/search-in-rotated-sorted-array.py
@@ -4,23 +4,25 @@ class Solution:
         left=0
         right=len(arr)-1
         
-        while left <=right :
+        while left<=right:
             mid=left+(right-left)//2
             
             if arr[mid]==key:
-                return mid  
+                return mid 
                 
             elif arr[left]<=arr[mid]:
                 if arr[left]<=key<arr[mid]:
                     right=mid-1
+                    
                 else:
                     left=mid+1
-            
+                    
             else:
                 if arr[mid]<key<=arr[right]:
                     left=mid+1
+                    
                 else:
                     right=mid-1
-        return -1
                     
             
+        return -1 

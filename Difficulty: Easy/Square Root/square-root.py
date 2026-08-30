3fd6eb980cd0faf1a594b534da939d192ad2d5a4
Nitheesvar10 <150546091+Nitheesvar10@users.noreply.github.com>
2026-08-30 17:35:13 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Square Root/square-root.py
+++ Difficulty: Easy/Square Root/square-root.py
@@ -1,20 +1,26 @@
 class Solution:
     def floorSqrt(self, n): 
         # code here
-        if n==1:
+        if n<=1:
             return 1
+            
         left=1
         right=n//2
         
         while left<=right:
             mid=left+(right-left)//2
             
-            if mid*mid==n:
-                return mid
+            sq=mid*mid
+            
+            if sq==n:
+                return mid 
                 
-            elif mid*mid >n:
+            elif sq>n:
                 right=mid-1
+                
             else:
                 left=mid+1
                 
+            
         return right 
+            

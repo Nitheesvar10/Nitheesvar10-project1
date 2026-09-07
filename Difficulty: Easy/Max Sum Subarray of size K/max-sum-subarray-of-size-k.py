bafeea741f946aeb667f37448d56d140f429e514
Nitheesvar10 <150546091+Nitheesvar10@users.noreply.github.com>
2026-09-07 22:26:17 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Max Sum Subarray of size K/max-sum-subarray-of-size-k.py
+++ Difficulty: Easy/Max Sum Subarray of size K/max-sum-subarray-of-size-k.py
@@ -3,17 +3,21 @@ class Solution:
         # code here 
         left=0
         right=0
+        max_sum=0
         cur_sum=0
-        max_sum=float("-inf")
         while right <len(arr):
             cur_sum+=arr[right]
             
-            while right-left+1>k:
+            while right-left+1 >k:
                 cur_sum-=arr[left]
+
                 left+=1
                 
-            max_sum=max(cur_sum,max_sum)
+            if right-left+1 ==k:
+                max_sum=max(max_sum,cur_sum)
+                
             right+=1
             
         return max_sum
+                
             

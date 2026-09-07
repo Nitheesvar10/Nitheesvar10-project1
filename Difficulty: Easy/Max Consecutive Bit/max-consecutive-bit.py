6cbc2460a2a2fc2dc61313afe34448480535e2e6
Nitheesvar10 <150546091+Nitheesvar10@users.noreply.github.com>
2026-09-07 22:28:44 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Max Consecutive Bit/max-consecutive-bit.py
+++ Difficulty: Easy/Max Consecutive Bit/max-consecutive-bit.py
@@ -3,17 +3,16 @@ class Solution:
         #code here 
         left=0
         right=0
-        cnt=0
         max_cnt=0
         while right <len(arr):
             
-            cnt-=1
-            
-            while arr[left]!=arr[right]:
-                cnt-=1
+            while arr[right]!=arr[left]:
                 left+=1
                 
             max_cnt=max(max_cnt,right-left+1)
             
             right+=1
-        return max_cnt 
+            
+        return max_cnt
+                
+            

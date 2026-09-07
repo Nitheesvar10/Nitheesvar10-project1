e5fd0b4458a73adf32a7d256f54642e855647e08
Nitheesvar10 <150546091+Nitheesvar10@users.noreply.github.com>
2026-09-07 22:40:17 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Max Consecutive 1's with K Flips/max-consecutive-1s-with-k-flips.py
+++ Difficulty: Medium/Max Consecutive 1's with K Flips/max-consecutive-1s-with-k-flips.py
@@ -5,21 +5,18 @@ class Solution:
         right=0
         zero_cnt=0
         max_cnt=0
-        
         while right <len(arr):
             if arr[right]==0:
                 zero_cnt+=1
                 
-            
-            while  zero_cnt >k:
+            while zero_cnt >k:
                 if arr[left]==0:
                     zero_cnt-=1
-                    
                 left+=1
+                
                 
             max_cnt=max(max_cnt,right-left+1)
             
+            
             right+=1
-            
         return max_cnt 
-            

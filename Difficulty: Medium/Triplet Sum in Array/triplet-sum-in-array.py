3201531980831723d58cf2abfd527b6bf6097362
Nitheesvar10 <150546091+Nitheesvar10@users.noreply.github.com>
2026-09-07 19:42:21 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Triplet Sum in Array/triplet-sum-in-array.py
+++ Difficulty: Medium/Triplet Sum in Array/triplet-sum-in-array.py
@@ -2,33 +2,37 @@ class Solution:
     def hasTripletSum(self, arr, target):
         # Code Here
         arr.sort()
-        for i in range(0,len(arr)-1):
-            if i>0 and arr[i]==arr[i-1]:
+        result=[]
+        for i in range(len(arr)-2):
+            if i >0 and arr[i]==arr[i-1]:
                 continue 
+            
             left=i+1
             right=len(arr)-1
-            while left<right:
+            
+            while left <right:
                 summ=arr[i]+arr[left]+arr[right]
                 
                 if summ==target:
-                    return True 
+                    result.append([arr[i],arr[left],arr[right]])
                     
-                    
-                    while left+1<right and arr[left] ==arr[left+1]:
+                    while left <right and arr[left]==arr[left+1]:
                         left+=1
-                    while right-1>left and arr[right]==arr[right-1]:
+                        
+                    while right >left  and arr[right]==arr[right-1]:
                         right-=1
                         
-                
-                elif summ>target:
+                    left+=1
                     right-=1
                     
-                    
-                    
+                elif summ >target :
+                    right-=1
                     
                 else:
                     left+=1
                     
-        return False 
-                    
-                
+            
+        if result:
+            return True  
+        else:
+            return False

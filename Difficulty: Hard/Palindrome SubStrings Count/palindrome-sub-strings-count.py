0ff0b33f0f95622bc58fd2a81f7923bd68449f24
Nitheesvar10 <150546091+Nitheesvar10@users.noreply.github.com>
2026-09-08 12:37:03 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Palindrome SubStrings Count/palindrome-sub-strings-count.py
+++ Difficulty: Hard/Palindrome SubStrings Count/palindrome-sub-strings-count.py
@@ -1,22 +1,21 @@
 class Solution:
     def countPS(self, s):
         # code here
-        self.count = 0
-
-        for i in range(len(s)):
-            self.count += self.pal(s, i, i)
-            self.count += self.pal(s, i, i + 1)
-
-        return self.count
-
-    def pal(self, s, left, right):
-        count = 0
-
-        while left >= 0 and right < len(s) and s[left] == s[right]:
-            if right - left + 1 >= 2:
-                count += 1
-
-            left -= 1
-            right += 1
-
-        return count
+        count=0
+        for i in range(0,len(s)):
+            count+=self.pal(s,i,i)
+            count+=self.pal(s,i,i+1)
+            
+        return count 
+        
+    def pal(self,s,left,right):
+        c=0
+        while (left >=0 and right <len(s)) and  (s[left]==s[right]):
+            if (right-left+1) >=2:
+                c+=1
+                
+            left-=1
+            right+=1
+            
+        return c 
+            

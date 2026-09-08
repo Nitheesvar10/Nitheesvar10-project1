11dda3ea136c660309e228fafc0040c249d4efe0
Nitheesvar10 <150546091+Nitheesvar10@users.noreply.github.com>
2026-09-08 12:16:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Palindrome String/palindrome-string.py
+++ Difficulty: Easy/Palindrome String/palindrome-string.py
@@ -3,14 +3,13 @@ class Solution:
         # code here
         left=0
         right=len(s)-1
-        
-        while left<right:
+        while left<right :
             if s[left]!=s[right]:
-                return False
+                return  False
                 
             left+=1
             right-=1
             
-                
         return True 
         
+ 

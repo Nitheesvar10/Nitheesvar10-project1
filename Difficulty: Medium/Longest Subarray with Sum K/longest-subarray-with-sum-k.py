0ecd8e132301e0807c85fdadc88c8ab2df16b624
Nitheesvar10 <150546091+Nitheesvar10@users.noreply.github.com>
2026-09-08 05:43:48 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Subarray with Sum K/longest-subarray-with-sum-k.py
+++ Difficulty: Medium/Longest Subarray with Sum K/longest-subarray-with-sum-k.py
@@ -1,22 +1,20 @@
 class Solution:
     def longestSubarray(self, arr, k):  
         # code here
-        cur_sum=0
         hash={0:-1}
-        max_len=0
-        for i in range(len(arr)):
+        max_lenght=0
+        cur_sum=0
+        for i in range(0,len(arr)):
             cur_sum+=arr[i]
             
             e=cur_sum-k
             
-            if e in hash:
-                max_len=max(max_len,i-hash[e])
+            if  e in hash:
+                max_lenght=max(max_lenght,i-hash[e])
                 
-            if cur_sum  not in hash:
+            if cur_sum not in hash:
                 hash[cur_sum]=i
-                
-        return max_len
-                
+            
+        return max_lenght
             
     
-            

b729ffc8bb1f6d016ab12fa3bcd57225c9cd85f1
Nitheesvar10 <150546091+Nitheesvar10@users.noreply.github.com>
2026-09-08 05:32:39 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Indexes of Subarray Sum/indexes-of-subarray-sum.py
+++ Difficulty: Medium/Indexes of Subarray Sum/indexes-of-subarray-sum.py
@@ -1,19 +1,20 @@
 class Solution:
     def subarraySum(self, arr, target):
         # code here
-        hash={0:-1}
         cur_sum=0
-        for i in range(len(arr)):
+        hash={0:-1}
+        
+        for i in range(0,len(arr)):
             cur_sum+=arr[i]
             
             e=cur_sum-target
-
+            
             if e in hash:
                 return [hash[e]+2,i+1]
-            
+                
             if cur_sum not in hash:
                 hash[cur_sum]=i
+                
             
         return [-1]
-                
-        
+            
